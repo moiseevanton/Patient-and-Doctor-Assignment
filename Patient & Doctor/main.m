29ef063ad4357c459b673858b8d42dc7f1ae4a2d
moiseevanton <athidif94@gmail.com>
2016-05-05 00:07:36 -0400

    Initial Commit

---
+++ Patient & Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient & Doctor
+//
+//  Created by Anton Moiseev on 2016-05-05.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
